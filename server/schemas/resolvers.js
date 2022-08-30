8845dabfa0445c0da70fedaed086cd87b6ca2f4e
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-29 19:41:12 -0700

    add resolver mutation createUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,5 +9,16 @@ const resolvers = {
                 $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
               }).populate('books');
         },
+    },
+
+    Mutation: {
+        createUser: async (parent, { username, email, password }) => {
+            //create a User document with the input parameters
+            const user = await User.create({ username, email, password });
+            //generate a token from this newly created User
+            const token = signToken(user);
+            //return an object with the token and the User document
+            return { token, user};
+        }
     }
 }
