b3f085e0b70e652bce7a425827ccfb0ab1df4812
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-29 22:53:35 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booksearch_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
