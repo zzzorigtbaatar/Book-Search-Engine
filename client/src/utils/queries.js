27f42c924747dd0778e6ea6c351dd56707ef1650
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-29 20:53:27 -0700

    fix queries GET_ME

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,9 +6,13 @@ export const GET_ME = gql`
       _id
       username
       email
+      bookCount
       savedBooks {
         _id
         bookId
+        authors
+        image
+        link
         title
         description
       }
