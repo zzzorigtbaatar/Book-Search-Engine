873f1065d0da3239190a35cc419d713c61e46e69
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-29 19:52:56 -0700

    add resolver mutation saveBook

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -38,5 +38,16 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    saveBook: async (parent, { user, body }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+        return {updatedUser};
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
   },
 };
