a7a1c981068b1926caf61737b938a9f68b985125
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-29 19:45:04 -0700

    add resolver mutation login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,24 +1,42 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        getSingleUser: async () => {
-            return User.findOne({
-                $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-              }).populate('books');
-        },
+  Query: {
+    getSingleUser: async () => {
+      return User.findOne({
+        $or: [
+          { _id: user ? user._id : params.id },
+          { username: params.username },
+        ],
+      }).populate("books");
     },
+  },
 
-    Mutation: {
-        createUser: async (parent, { username, email, password }) => {
-            //create a User document with the input parameters
-            const user = await User.create({ username, email, password });
-            //generate a token from this newly created User
-            const token = signToken(user);
-            //return an object with the token and the User document
-            return { token, user};
-        }
-    }
-}
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      //create a User document with the input parameters
+      const user = await User.create({ username, email, password });
+      //generate a token from this newly created User
+      const token = signToken(user);
+      //return an object with the token and the User document
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
